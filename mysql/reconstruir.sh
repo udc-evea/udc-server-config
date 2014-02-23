a57498b8cb22a91e4bf69903c04fb6b3cc192060
mppfiles <mppfiles@gmail.com>
2014-02-23 18:50:33 -0300

    agrego script para reconstruir esclavo

---
+++ mysql/reconstruir.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# detiene la replicación 
+/opt/lampp/bin/mysqladmin -u root -p stop-slave
+
+# trae el último backup de la base
+ssh -C test.udc.edu.ar "/opt/lampp/bin/mysqldump -p  --master-data --add-drop-database --opt --compress --databases moodle | gzip -9 -c" > moodle.sql.gz
+
+# restaura el backup
+gunzip -c moodle.sql.gz | mysql -u root -p
+
+# reinicia la replicación
+/opt/lampp/bin/mysqladmin -u root -p start-slave
