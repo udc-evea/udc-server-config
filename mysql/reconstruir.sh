7e77e79ac237cd4a5410be3fa1bdc0a208d685a5
mppfiles <mppfiles@gmail.com>
2014-02-23 18:40:16 -0300

    mejorando mensajes en reconstruir

--- mysql/reconstruir.sh
+++ mysql/reconstruir.sh
@@ -2,22 +2,22 @@
 
 set -e
 # detiene la replicación 
-echo "BASE LOCAL: detener replicación."
-/opt/lampp/bin/mysqladmin -u root -p stop-slave
+echo "Deteniendo replicación en servidor esclavo..."
+/opt/lampp/bin/mysqladmin --login-path=local stop-slave
 
 # trae el último backup de la base
-echo "BASE REMOTA: recuperar backup."
-ssh -C test.udc.edu.ar "/opt/lampp/bin/mysqldump -p  --master-data --add-drop-database --opt --compress --databases moodle | gzip -9 -c" > moodle.sql.gz
+echo "Generando backup remoto..."
+ssh -C test.udc.edu.ar "/opt/lampp/bin/mysqldump --login-path=backup  --master-data --add-drop-database --opt --compress --databases moodle | gzip -9 -c" > moodle.sql.gz
 
 # restaura el backup
-echo "BASE LOCAL: restaurar backup."
-gunzip -c moodle.sql.gz | /opt/lampp/bin/mysql -u root -p
+echo "Restaurando backup en base local. Por favor espere..."
+gunzip -c moodle.sql.gz | /opt/lampp/bin/mysql --login-path=local
 
 # reinicia la replicación
-echo "BASE LOCAL: reanudar replicación."
-/opt/lampp/bin/mysqladmin -u root -p start-slave
+echo "Reanudando replicación..."
+/opt/lampp/bin/mysqladmin --login-path=local start-slave
 
 # elimino el backup
-rm moodle.sql.gz
+rm -f moodle.sql.gz
 
 echo "PROCESO COMPLETO."
